1f3dabd202ae74e513fe079d83f7ea932aaa25b4
Victor Schweitz <victorschweitz@hotmail.com>
2025-01-02 12:22:51 +0100

    Board randomized

--- js/main.js
+++ js/main.js
@@ -1,45 +1,73 @@
-import { cards } from './cards.js';
-console.log(cards);
+import { cards } from './cards.js'
 
-const cardsContainer = document.querySelector('.cards-container');
+const cardsContainer = document.querySelector('.cards-container')
 
-cardsContainer.addEventListener('click', (e) => flipCard(e));
+// This will store the generated card elements
+let cardsArray = []
 
-function generateCards()
+window.addEventListener('load', () =>
 {
-    cards.forEach((card) =>
+    duplicateCards()
+    randomizeBoard()
+})
+
+cardsContainer.addEventListener('click', (e) => flipCard(e))
+
+// Generate Cards
+function generateCards(cardsData)
+{
+    const cardElements = []
+    cardsData.forEach((card) =>
     {
-        // Use += to append cards instead of overwriting
-        cardsContainer.innerHTML += `
+        const cardElement = `
         <div class="card memory-card">
             <div class="memory-card-inner">
                 <div class="memory-card-front">
-                  ${card.cardFront}
+                    ${card.cardFront}
                 </div>
                 
                 <div class="memory-card-back">
-                    <img src=${card.cardBack} alt="">
+                    <img src="${card.cardBack}" alt="">
                 </div>
             </div>
-        </div>
-        `;
-    });
+        </div>`
+        cardElements.push(cardElement)
+    })
+    return cardElements // Return generated elements
 }
 
-for(let i = 0; i<2; i++)
+// Duplicate cards and generate two rows of cards
+function duplicateCards()
 {
-    generateCards();
+    const cardRows = [...cards, ...cards] // Duplicate the array
+    cardsArray = generateCards(cardRows) // Store generated card elements in cardsArray
 }
 
+// Flip card
 function flipCard(e)
 {
-    // Traverse the DOM to find the closest card
-    const memoryCard = e.target.closest('.memory-card');
+    const memoryCard = e.target.closest('.memory-card')
     if (memoryCard)
     {
-        // Toggle the class on the "memory-card-inner"
-        const memoryCardInner = memoryCard.querySelector('.memory-card-inner');
-        memoryCardInner.classList.toggle('selected');
-        console.log('Card flipped:', memoryCardInner);
+        const memoryCardInner = memoryCard.querySelector('.memory-card-inner')
+        memoryCardInner.classList.toggle('selected')
     }
+}
+
+// Randomize cards and render them
+function randomizeBoard()
+{
+    // Fisher-Yates Shuffle on cardsArray
+    for (let i = cardsArray.length - 1; i > 0; i--)
+    {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [cardsArray[i], cardsArray[randomIndex]] = [cardsArray[randomIndex], cardsArray[i]]
+    }
+
+    // Clear the container and append shuffled cards
+    cardsContainer.innerHTML = '' // Clear previous cards
+    cardsArray.forEach((cardElement) =>
+    {
+        cardsContainer.innerHTML += cardElement // Append each shuffled card
+    })
 }
